4e33335ae0f819dee11e41fe9328e04e7f68f4af
gameyuki <yuki.game091425@gmail.com>
2023-05-06 20:25:45 +0900

    本日分終了

--- 機械学習練習/test2.py
+++ 機械学習練習/test2.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import make_blobs
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 X,y = make_blobs(
     random_state=0,
@@ -9,14 +10,29 @@ X,y = make_blobs(
     cluster_std=1,
     n_samples=300)
 
-df = pd.DataFrame(X)
-df["target"] = y
-print(df.head())
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+
+df = pd.DataFrame(X_train)
+df["target"] = y_train
 
 df0 = df[df["target"] == 0]
 df1 = df[df["target"] == 1]
-
 plt.figure(figsize=(5, 5))
+
 plt.scatter(df0[0], df0[1], color="b", alpha=0.5)
 plt.scatter(df1[0], df1[1], color="r", alpha=0.5)
+plt.title("title:75%")
 plt.show()
+
+df = pd.DataFrame(X_test)
+df["target"] = y_test
+
+df0 = df[df["target"] == 0]
+df1 = df[df["target"] == 1]
+plt.figure(figsize=(5, 5))
+
+plt.scatter(df0[0], df0[1], color="b", alpha=0.5)
+plt.scatter(df1[0], df1[1], color="r", alpha=0.5)
+plt.title("test:25%")
+plt.show()
+
